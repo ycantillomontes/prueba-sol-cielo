aab3a42fd4c6c8ce1d72e7e17943038f3ac13e6c
Yesith <cyesid79@gmail.com>
2026-08-26 00:28:20 -0500

    feat: configurar admin y CORS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pqrs',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -126,3 +128,7 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
--- backend/pqrs/admin.py
+++ backend/pqrs/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TicketPQRS
+
+
+@admin.register(TicketPQRS)
+class TicketPQRSAdmin(admin.ModelAdmin):
+    list_display = (
+        "ticket_code",
+        "applicant_name",
+        "applicant_email",
+        "category",
+        "subject",
+        "status",
+        "created_at",
+    )
+
+    list_filter = (
+        "category",
+        "status",
+        "created_at",
+    )
+
+    search_fields = (
+        "ticket_code",
+        "applicant_name",
+        "applicant_email",
+        "subject",
+    )
+
+    readonly_fields = (
+        "ticket_code",
+        "created_at",
+    )
